a5a01477329bc1c226ddd55223afa71307445f50
Tim <timjewing@gmail.com>
2020-07-29 13:30:40 -0600

    Mass update - currently working version

--- omnichrome.py
+++ omnichrome.py
@@ -8,12 +8,19 @@ import colorsys
 from PIL import Image
 import numpy as np
 
+### HERE BE DRAGONS
+# Let it be known that we may get segfaults if we recur too hard
+import resource
+import sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
+### </dragons>
+
 _INF = float('infinity')
 _SIZE = None
 
-## Some style notes:
-# rgb and xy tuples can be concat'd down, so (r, g, b) is (r,g,b) etc.
-# line wrapping is at 100 chars (meaning only 99 chars per line)
+## Style note:
+# line wrapping is at 100 chars (meaning only 99 chars per line) if I remember
 @total_ordering
 class OmniPix():
     def __init__(self, r, g, b):
@@ -54,18 +61,39 @@ class OmniPix():
         return str(self.color) + ' ' + str(self.potential)
 
     def dist(self, other):
-        ## Easy sum of distances distance for now, maybe do euclidian later
+        # This function is a pretty abstract definition of 'color distance'
+        # changing it pretty radically changes the output
+        if self == other:
+            return 0
         d = (
-            20 * min([
+            # Hue is circular; take the shorter distance
+            min([
+                (self.hsv[0] - other.hsv[0]) % _SIZE,
+                (other.hsv[0] - self.hsv[0]) % _SIZE,
+            ])
+                * (self.hsv[1] / _SIZE) # Scale hue by normalized shade
+                * (other.hsv[1] / _SIZE) # Repeat for the hue of the comparison color
+                * 6
+            + abs(self.hsv[1] - other.hsv[1]) * 3 # Add difference in shade
+            + abs(self.hsv[2] - other.hsv[2]) # Add difference in value
+        )
+        return d
+
+    # Separate function for setting potentials
+    def get_potential(self, other):
+        # Potentials used for slope-descent chunk assignment
+        d = (
+            min([
                 (self.hsv[0] - other.hsv[0]) % _SIZE,
                 (other.hsv[0] - self.hsv[0]) % _SIZE,
             ])
             + abs(self.hsv[1] - other.hsv[1])
             + abs(self.hsv[2] - other.hsv[2])
         )
-        return d if d != 0 else 1e-3
+        return 1/d if d != 0 else _INF
 
     def _hsv(self):
+        # Get this color as HSV
         h,s,v = colorsys.rgb_to_hsv(
             self.color[0] / _SIZE, 
             self.color[1] / _SIZE, 
@@ -78,7 +106,7 @@ class OmniPix():
         )
 
 class OmniCluster():
-    def __init__(self, omni_pixels, chunk_pix, chunk_size=20, depth=0):
+    def __init__(self, omni_pixels, chunk_pix, chunk_size=16, depth=0):
         self.omni_pixels = omni_pixels
         self.chunk_pix = chunk_pix
         self.chunk_size = chunk_size
@@ -104,28 +132,60 @@ class OmniCluster():
             return
         # Pick centroids
         # This could be replaced with a better selection method for a more balanced tree
-        centroids = random.sample(self.omni_pixels, self.chunk_size)
+        # This method basically does random.sample(self.omni_pixels, self.chunk_size)
+        # but pops the selected pixels from the array because efficiency
+        # TODO: Rewrite this to actually be readable (sorry)
+        if self.depth == 0:
+            print("          - Picking centroids")
+        centroids = [
+            self.omni_pixels.pop(x) # Pop element x
+            for x in sorted(random.sample(
+                range(len(self.omni_pixels)), # Pick x from range(0, len(self.omni_pixels))
+                self.chunk_size # pick self.chunk_size samples
+            ))[::-1]
+        ]
+
         # Initialize centroid assignments
         centroid_assignments = {}
         for centroid in centroids:
-            centroid_assignments[centroid] = []
+            centroid_assignments[centroid] = [centroid]
         # Assign each pixel to a centroid
+        if self.depth == 0:
+            print("          - Assinging pixels to centroids")
         for omni_pix in self.omni_pixels:
             centroid = min(centroids, key=omni_pix.dist)
             centroid_assignments[centroid].append(omni_pix)
+
         # Build tree
         self.tree = {}
+        centroid_count = 0
         for centroid in centroid_assignments:
-           self.tree[centroid] = OmniCluster(
-            centroid_assignments[centroid], 
-            self.chunk_pix,
-            self.chunk_size,
-            depth=self.depth+1,
-        )
+            if self.depth == 0:
+                centroid_count += 1
+                print(
+                    "          - Building branch {} of {}  ".format(centroid_count,self.chunk_size),
+                    end='\r'
+                )
+            try:
+                self.tree[centroid] = OmniCluster(
+                    centroid_assignments[centroid], 
+                    self.chunk_pix,
+                    self.chunk_size,
+                    depth=self.depth+1,
+                )
+            except RecursionError:
+                # It's bad news if we get here, but since we're doing random center selection
+                # it can actually happen randomly. Let's hope it doesn't.
+                print("Recursion Depth Exceeded:", self.depth + 1)
+                exit()
+        if self.depth == 0:
+            print()
 
     def pop(self, target):
+        # This is a (complicated) method for popping off the closest node to the target
         if self.tree is None:
-            # If this is the 'stem' (the last node from the bottom), just get the closest leaf
+            # If this node is a 'stem' (it has leafs, but those leaf nodes have no children), then
+            # return the closest match among the leafs
             if len(self.leafs) == 0:
                 return None
             node = min(self.leafs, key=lambda x: target.dist(x))
@@ -151,47 +211,56 @@ class OmniChunk():
         self.chunk_pix = chunk_pix
         self.tree = None
 
-    def build(self, omni_cube, count):
-        # Select the pixels that will go in this chunk
-        selected_pix = []
-        ## Use a set to track which pixels have been marked as adjacent and a heap to track their 
-        # ordering by potential
-        adj_pixels_set = set()
-        adj_pixels_heap = []
+    def build(self, omni_cube, count, last=False):
+        if last:
+            # If there is only one chunk left, don't get fancy - just select everything not yet 
+            # selected
+            selected_pix = [x for x in omni_cube.cube if not x.selected]
+        else:
+            # Select the pixels that will go in this chunk
+            selected_pix = []
+            ## Use a set to track which pixels have been marked as adjacent and a heap to track 
+            # their ordering by potential
+            adj_pixels_set = set()
+            adj_pixels_heap = []
 
-        current_pix = omni_cube.cube[self.chunk_pix]
-        while len(selected_pix) < count:
-            # Add this pixel to selected 
-            selected_pix.append(current_pix)
-            current_pix.selected = True
-            # Add adjacent pixels to heapq/set
-            for adj_pix in omni_cube.adj(current_pix):
-                if adj_pix in adj_pixels_set:
-                    pass
+            current_pix = omni_cube.cube[self.chunk_pix]
+            while len(selected_pix) < count:
+                # Add this pixel to selected 
+                selected_pix.append(current_pix)
+                current_pix.selected = True
+                # Add adjacent pixels to heapq/set
+                for adj_pix in omni_cube.adj(current_pix):
+                    if adj_pix in adj_pixels_set:
+                        pass
+                    else:
+                        adj_pixels_set.add(adj_pix)
+                        # Push the pixel on to the heap as a (potential, pixel) tuple, subtracting
+                        # off the potential from this chunk so we actually follow a reasonable 
+                        # contour
+                        heappush(
+                            adj_pixels_heap, 
+                            (adj_pix.potential - adj_pix.get_potential(self.chunk_pix), adj_pix)
+                        )
+                # Find next pixel
+                if len(adj_pixels_set) > 0:
+                    potential, current_pix = heappop(adj_pixels_heap)
+                    # Remove selected pixel from set
+                    adj_pixels_set.discard(current_pix)
                 else:
-                    adj_pixels_set.add(adj_pix)
-                    # Push the pixel on to the heap as a (potential, pixel) tuple, subtracting off
-                    # the potential from this chunk so we actually follow a reasonable contour
-                    heappush(
-                        adj_pixels_heap, 
-                        (adj_pix.potential - (1 / adj_pix.dist(self.chunk_pix)), adj_pix)
+                    # If we ran out of adjacents, pick the closest color and keep going unless we're 
+                    # done filling this chunk
+                    if len(selected_pix) == count:
+                        break
+                    current_pix = min(
+                        [x for x in omni_cube.cube if x.potential < _INF and not x.selected], 
+                        key=self.chunk_pix.dist,
                     )
-            # Find next pixel
-            if len(adj_pixels_set) > 0:
-                potential, current_pix = heappop(adj_pixels_heap)
-                # Remove selected pixel from set
-                adj_pixels_set.discard(current_pix)
-            else:
-                # If we ran out of adjacents, pick the closest color and keep going unless we're 
-                # done filling this chunk
-                if len(selected_pix) == count:
-                    break
-                current_pix = min(
-                    [x for x in omni_cube.cube if x.potential < _INF and not x.selected], 
-                    key=self.chunk_pix.dist,
-                )
+
         # Construct a k-means cluster tree
+        print("      - Building tree", self.chunk_pix.color)
         self.tree = OmniCluster(selected_pix, self.chunk_pix)
+
 
     def pop(self, omni_pix):
         return self.tree.pop(omni_pix)
@@ -223,20 +292,19 @@ class OmniCube():
         for _, color in self.cube.items():
             potential = 0
             for chunk_pix in self.chunk_pixels:
-                # potential is 1/x where x is the distance
-                # gets set to inf at chunk colors so they should never get stolen by another chunk
-                if color == chunk_pix:
-                    potential = _INF
-                    break
-                potential += 1 / color.dist(chunk_pix)
+                potential += color.get_potential(chunk_pix)
             color.potential = potential
 
     def build_chunks(self):
         for chunk_pix in self.chunk_pixels:
             self.chunks[chunk_pix] = OmniChunk(chunk_pix)
-        for chunk_color, chunk in self.chunks.items():
+        for chunk_num, (chunk_color, chunk) in enumerate(self.chunks.items()):
+            if chunk_num == len(self.chunk_pixels) - 1:
+                last = True
+            else:
+                last = False
             print("  - Building chunk {}".format(chunk_color.color))
-            chunk.build(self, self.chunk_sizes[chunk_color])
+            chunk.build(self, self.chunk_sizes[chunk_color], last=last)
 
     def adj(self, color):
         r, g, b = color.color
@@ -273,19 +341,23 @@ def save_image(filename, size, arr):
     print("Saved to {}".format(filename))
 
 if __name__ == '__main__':
-    _SIZE = 64
+    _SIZE = 128
     print("Opening image")
-    input_image = get_image('darkside_64.jpg', _SIZE)
-    input_map = get_image('darkside_64_map.png', _SIZE)
-    print(input_image.shape, input_map.shape)
-    assert input_image.shape == input_map.shape
+    input_image = get_image('green_black_128.png', _SIZE)
+    input_map = get_image('green_black_128.png', _SIZE)
+    print("  - Image shape:", input_image.shape)
+    try:
+        assert input_image.shape == input_map.shape
+        assert input_image.shape[2] == 3
+    except AssertionError:
+        raise
     width, height, _ = input_image.shape
 
     print("Assigning chunks")
     input_map_reshaped = input_map.reshape(-1, input_map.shape[2]) # flat map
     chunks = np.unique(input_map_reshaped, axis=0, return_counts=True)
     chunks = list(zip(*chunks)) # make (color, count) tuples
-    chunks = [(tuple(c), s) for c,s in chunks][::-1]
+    chunks = [(tuple(c), s) for c,s in chunks]
     print("  - Found {} chunks".format(len(chunks)))
     chunk_map = {}
     for x in range(width):
@@ -296,24 +368,36 @@ if __name__ == '__main__':
     print("Building cube")
     cube = OmniCube(_SIZE, chunks)
 
-    for x in range(width):
-        print("Assigning new colors: {:.2f}%".format((x/width) * 100), end='\r')
-        for y in range(height):
-            target_color_list = []
-            if x > 0:
-                target_color_list.append(tuple(input_image[x-1,y]))
-            if y > 0:
-                target_color_list.append(tuple(input_image[x,y-1]))
-            if x < width-2:
-                target_color_list.append(tuple(input_image[x+1,y]))
-            if y < height-2:
-                target_color_list.append(tuple(input_image[x,y+1]))
-            for _ in range(1):
-                target_color_list.append(tuple(input_image[x,y]))
-            target_color_channels = zip(*target_color_list)
-            target_color = [sum(y) // len(y) for y in target_color_channels]
-            input_image[x,y] = cube.pop(chunk_map[(x,y)], target_color).color
-            # input_image[x,y] = cube.pop(chunk_map[(x,y)], tuple(input_image[x,y])).color
-    print("")
+    coords = [(x,y) for x in range(width) for y in range(height)]
+    def key(coord):
+        return -(coord[0] - width/2) ** 2 + -(coord[1] - height/2) ** 2
+    coords = sorted(coords, key=key)
+    # coords[1497274:] = sorted(coords[1497274:])
+
+    i = 0
+    for x, y in coords:
+        if i % 1000 == 0:
+            print("Assigning new colors: {:.2f}%  ".format((i/len(coords)) * 100), end='\r')
+        i += 1
+
+        target_color_list = []
+        if x > 0:
+            # if chunk_map[(x,y)] == chunk_map[(x-1,y)]:
+            target_color_list.append(tuple(input_image[x-1,y]))
+        if y > 0:
+            # if chunk_map[(x,y)] == chunk_map[(x,y-1)]:
+            target_color_list.append(tuple(input_image[x,y-1]))
+        if x < width-2:
+            # if chunk_map[(x,y)] == chunk_map[(x+1,y)]:
+            target_color_list.append(tuple(input_image[x+1,y]))
+        if y < height-2:
+            # if chunk_map[(x,y)] == chunk_map[(x,y+1)]:
+            target_color_list.append(tuple(input_image[x,y+1]))
+        for _ in range(1):
+            target_color_list.append(tuple(input_image[x,y]))
+        target_color_channels = zip(*target_color_list)
+        target_color = [sum(chans) // len(chans) for chans in target_color_channels]
+        input_image[x,y] = cube.pop(chunk_map[(x,y)], target_color).color
+    print("\nDone")
     # save output
     save_image('out.bmp', _SIZE, input_image)
